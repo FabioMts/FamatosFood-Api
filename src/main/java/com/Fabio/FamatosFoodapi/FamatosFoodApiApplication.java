1734e18a4cfd36b37ed2a8bd890af298d23635c4
Fabio Matos <fabio.thunder@hotmail.com>
2022-07-27 16:39:41 -0300

    Criação do projeto

---
+++ src/main/java/com/Fabio/FamatosFoodapi/FamatosFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.Fabio.FamatosFoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamatosFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamatosFoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Fabio/FamatosFoodapi/FamatosFoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fabio.FamatosFoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamatosFoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
