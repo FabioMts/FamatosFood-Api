1e03daad2dad4f84bcbf22cf961ae76596d26cea
Fabio Matos <fabio.thunder@hotmail.com>
2022-12-29 19:01:53 -0300

    atualizando um objeto no banco de dados

---
+++ src/main/java/com/Fabio/FamatosFoodapi/jpa/AlteracaoCozinhaMain.java
@@ -0,0 +1,28 @@
+package com.Fabio.FamatosFoodapi.jpa;
+
+import com.Fabio.FamatosFoodapi.FamatosFoodApiApplication;
+import com.Fabio.FamatosFoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+public class AlteracaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(FamatosFoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
+
+        cadastroCozinha.adicionar(cozinha);
+
+
+    }
+
+}
