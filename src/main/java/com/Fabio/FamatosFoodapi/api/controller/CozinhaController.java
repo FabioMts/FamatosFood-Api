1e7d610036f711b5266f95a7d380b48a9c4854a1
Fabio Matos <fabio.thunder@hotmail.com>
2023-03-08 21:47:44 -0300

    consultando Singleton Resource com GET e @PathVariable

--- src/main/java/com/Fabio/FamatosFoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/Fabio/FamatosFoodapi/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import com.Fabio.FamatosFoodapi.domain.model.Cozinha;
 import com.Fabio.FamatosFoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.todas();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
+        return cozinhaRepository.porId(id);
     }
 
 }
