6cc55518328a30bdd17d910e998c57d80a3d620a
Fabio Matos <fabio.thunder@hotmail.com>
2023-04-05 17:45:36 -0300

    adicionando endpoint de Post e Put ao controlador de Cozinha

--- src/main/java/com/Fabio/FamatosFoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/Fabio/FamatosFoodapi/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.Fabio.FamatosFoodapi.api.controller;
 
 import com.Fabio.FamatosFoodapi.domain.model.Cozinha;
 import com.Fabio.FamatosFoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +28,27 @@ public class CozinhaController {
 
         if(cozinha != null) {
             return ResponseEntity.ok(cozinha);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+        return cozinhaRepository.adicionar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
+                                             @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.porId(cozinhaId);
+
+
+        if(cozinhaAtual != null) {
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+
+            cozinhaAtual = cozinhaRepository.adicionar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
         }
         return ResponseEntity.notFound().build();
     }
