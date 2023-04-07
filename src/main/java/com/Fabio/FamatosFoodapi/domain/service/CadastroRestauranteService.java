8a4236931899f807b69e314e93c93fb37900099d
Fabio Matos <fabio.thunder@hotmail.com>
2023-04-07 18:04:10 -0300

    implementando método post para entidade restaurante

---
+++ src/main/java/com/Fabio/FamatosFoodapi/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,34 @@
+package com.Fabio.FamatosFoodapi.domain.service;
+
+
+import com.Fabio.FamatosFoodapi.domain.exception.EntidadeNaoEncontradaException;
+import com.Fabio.FamatosFoodapi.domain.model.Cozinha;
+import com.Fabio.FamatosFoodapi.domain.model.Restaurante;
+import com.Fabio.FamatosFoodapi.domain.repository.CozinhaRepository;
+import com.Fabio.FamatosFoodapi.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroRestauranteService {
+
+    @Autowired
+    RestauranteRepository restauranteRepository;
+
+    @Autowired
+    CozinhaRepository cozinhaRepository;
+
+    public Restaurante salvar(Restaurante restaurante) {
+
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Cozinha cozinha = cozinhaRepository.porId(cozinhaId);
+
+        if(cozinha == null) {
+            throw new EntidadeNaoEncontradaException(String.format("Não existe cadastro de cozinha com o código %d.", cozinhaId));
+        }
+        restaurante.setCozinha(cozinha);
+        return  restauranteRepository.adicionar(restaurante);
+    }
+
+
+}
